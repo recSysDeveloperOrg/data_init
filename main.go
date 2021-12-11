7bc9e1f8ae6d5b183f7af783131a44be7358bc02
lijunyu <lijunyu@ljygogogo.com>
2021-12-11 23:11:59 +0800

    base scrape functions

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"data_init/config"
 	"data_init/model"
+	"data_init/scrape"
 )
 
 const (
 	RatingsFile = "ml-latest/ratings.csv"
 	TagFile     = "ml-latest/tags.csv"
+	LinkFile    = "ml-latest/links.csv"
 )
 
 func main() {
@@ -24,6 +26,9 @@ func main() {
 		panic(err)
 	}
 	if err := model.DoTagModels(TagFile); err != nil {
+		panic(err)
+	}
+	if err := scrape.DoScrape(LinkFile, RatingsFile); err != nil {
 		panic(err)
 	}
 }
